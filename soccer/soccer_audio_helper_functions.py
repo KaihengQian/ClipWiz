04c854cbca5419aa829472b7dea14fa9e763ed54
KaihengQian <2980811196@qq.com>
2023-01-27 19:00:36 +0800

    update video_editor.py

--- soccer/soccer_audio_helper_functions.py
+++ soccer/soccer_audio_helper_functions.py
@@ -26,45 +26,4 @@ def audio_energy(audio, sample_number, block_number, block_audio_info_path):
     block_audio_info.to_csv(block_audio_info_path)
 
 
-def get_time_list(block_audio_info_path, time_list_path):
-    block_audio_info = pd.read_csv(block_audio_info_path)
-    time_list = pd.DataFrame(columns=['start', 'end'])
-
-    # 根据得分筛选得到所需块
-    score = np.array(block_audio_info['score'])
-    score.sort()
-    threshold_rate = 0.96  # 设置阈值以筛选得分最高的4%的块
-    threshold = score[math.ceil(threshold_rate * len(score))]
-    row_index = 0
-    for i in range(len(block_audio_info)):
-        if block_audio_info.loc[i, 'score'] >= threshold:
-            time_list.loc[row_index, 'start'] = block_audio_info.loc[i, 'start']
-            time_list.loc[row_index, 'end'] = block_audio_info.loc[i, 'end']
-            row_index += 1
-
-    # 合并时间相邻的块，更新时间序列
-    temp = []
-    i = 0
-    j = 0
-    m = len(time_list) - 2
-    n = len(time_list) - 1
-    while i <= m:
-        j = i + 1
-        while j <= n:
-            if time_list.loc[i, 'end'] == time_list.loc[j, 'start']:
-                time_list.loc[i, 'end'] = time_list.loc[j, 'end']
-                temp.append(j)
-                j += 1
-            else:
-                i = j
-                break
-    time_list.drop(temp, inplace=True)
-    time_list.reset_index(drop=True)
-
-    # 将每个块的开始时间提早5秒
-    for i in range(len(time_list)):
-        if time_list.loc[i, 'start'] >= 5:
-            time_list.loc[i, 'start'] -= 5
-
-    time_list.to_csv(time_list_path)
 
--- video-editor/video_editor.py
+++ video-editor/video_editor.py
@@ -1,6 +1,51 @@
 import numpy as np
 import pandas as pd
+import math
 from moviepy import editor
+
+
+def get_time_list(block_audio_info_path, time_list_path):
+    block_audio_info = pd.read_csv(block_audio_info_path)
+    time_list = pd.DataFrame(columns=['start', 'end'])
+
+    # 根据得分筛选得到所需块
+    score = np.array(block_audio_info['score'])
+    score.sort()
+    threshold_rate = 0.96  # 设置阈值以筛选得分最高的4%的块
+    threshold = score[math.ceil(threshold_rate * len(score))]
+    row_index = 0
+    for i in range(len(block_audio_info)):
+        if block_audio_info.loc[i, 'score'] >= threshold:
+            time_list.loc[row_index, 'start'] = block_audio_info.loc[i, 'start']
+            time_list.loc[row_index, 'end'] = block_audio_info.loc[i, 'end']
+            row_index += 1
+
+    # 合并时间相邻的块，更新时间序列
+    temp = []
+    i = 0
+    j = 0
+    m = len(time_list) - 2
+    n = len(time_list) - 1
+    while i <= m:
+        j = i + 1
+        while j <= n:
+            if time_list.loc[i, 'end'] == time_list.loc[j, 'start']:
+                time_list.loc[i, 'end'] = time_list.loc[j, 'end']
+                temp.append(j)
+                j += 1
+            else:
+                i = j
+                break
+    time_list.drop(temp, inplace=True)
+    time_list.reset_index(drop=True)
+
+    # 考虑反应时间，将块的开始时间相应提前
+    response_time = 5
+    for i in range(len(time_list)):
+        if time_list.loc[i, 'start'] >= response_time:
+            time_list.loc[i, 'start'] -= response_time
+
+    time_list.to_csv(time_list_path)
 
 
 def video_edit(input_video_path, time_list_path, output_video_path):
